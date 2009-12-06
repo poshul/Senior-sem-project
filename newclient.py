7eebc4d31903cb69bd0efcf0e3a443518a36b99a
Samuel Wein <spwein06@earlham.edu>
2009-12-06 15:53:30 -0500

    Added try except statements to reestablish communication with other servers.

--- newclient.py
+++ newclient.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 mySocket = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
 mySocket.bind ( ( '', 2487 ) )
 mySocket.listen ( 1 )
@@ -7,17 +8,24 @@ while True:
     print 'We have opened a connection with', details
     while True:
         try:
-            print channel.recv(100)
+            received=channel.recv(100)
+            if received!='': #dont print junk when we are establishing new connection
+                print "received", received
+        except socket.error:
+            channel,details =mySocket.accept()
+            print "opened a new socket with",details
         except:
-            print "recv error"
+            print "recv error",sys.exc_info()
             channel.close()
             mySocket.close()
             quit()
-        print "received"
         try:
             channel.send("next")
+        except socket.error:
+            channel,details =mySocket.accept()
+            print "opened a new socket with",details
         except:
-            print "send error"
+            print "send error",sys.exc_info()
             channel.close()
             mySocket.close()
             quit()
